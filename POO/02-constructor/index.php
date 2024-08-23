a2d919f313f8ae8451a6e6ffa193e4d7ebca390c
David Murillo <david.josue.murillo23@gmail.com>
2024-08-22 23:47:45 -0500

    Constructor

---
+++ POO/02-constructor/index.php
@@ -0,0 +1,45 @@
+<?php 
+// Programación orientada a objetos
+
+class Coche {
+    
+    // Atributos
+    public $brand; // Marca
+    public $model; // Modelo
+    public $color;
+    public $speed; // Velocidad
+
+
+    // Métodos
+    public function getColor() {
+        return $this->color;
+    }
+
+    public function setColor($color) {
+        $this->color = $color;
+    }
+
+    public function accelerate() {
+        $this->speed++;
+    }
+
+    public function brake() {
+        $this->speed--;
+    }
+ 
+
+    public function getSpeed() {
+        return $this->speed;
+    }
+}
+
+// Instancia
+$mycar = new Coche();
+
+// Usando metodos
+$mycar->accelerate();
+$mycar->setColor('rojo');
+
+// Usando atributos
+echo $mycar->getColor();
+echo $mycar->getSpeed();
