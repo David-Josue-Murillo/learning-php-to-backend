062b7908b98ac2a96829fbee8503cc8330333e8d
David Murillo <david.josue.murillo23@gmail.com>
2024-08-22 23:25:11 -0500

    First Commit

---
+++ POO/01-clases/index.php
@@ -0,0 +1,34 @@
+<?php 
+// Programación orientada a objetos
+
+class Coche {
+    
+    // Atributos
+    public $brand; // Marca
+    public $model; // Modelo
+    public $color;
+    public $speed; // Velocidad
+
+
+    // Métodos
+    public function getColor() {
+        return $this->color;
+    }
+
+    public function setColor($color) {
+        $this->color = $color;
+    }
+
+    public function accelerate() {
+        $this->speed++;
+    }
+
+    public function brake() {
+        $this->speed--;
+    }
+ 
+
+    public function getSpeed() {
+        return $this->speed;
+    }
+}
