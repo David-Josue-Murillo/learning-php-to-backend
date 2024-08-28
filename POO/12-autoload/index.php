0616a1868b07d3473034a8449333bec6254eeb55
David Murillo <david.josue.murillo23@gmail.com>
2024-08-28 14:54:03 -0500

    NameSpace

--- POO/12-autoload/index.php
+++ POO/12-autoload/index.php
@@ -2,7 +2,7 @@
 
 require_once 'autoload.php';
 
-$user = new Usuario();
+/*$user = new Usuario();
 echo $user->nombre. "<br/>";
 
 $categoria = new Categoria("Reviews");
@@ -11,3 +11,17 @@ echo $categoria->nombre. "<br/>";
 $entrada = new Entrada();
 echo $entrada->titulo. "<br/>";
 echo $entrada->fecha. "<br/>";
+ */
+
+ class Principal {
+    public$usuario;
+    public $categoria;
+    public $entrada;
+
+    public function __construct() {
+        $this->usuario = new MisClases\Usuario();
+    }
+ }
+
+ $principal = new Principal();
+ var_dump($principal->usuario);
