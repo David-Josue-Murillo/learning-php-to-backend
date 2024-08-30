909f3f43b6dd2766ef4b9fdfa522be32e19b55d3
David Murillo <david.josue.murillo23@gmail.com>
2024-08-30 16:35:14 -0500

    Control the pages

--- Store Clothes/config/db.php
+++ Store Clothes/config/db.php
@@ -7,4 +7,4 @@ class Database {
 
         return $db;
     }  
-}
+}   
--- Store Clothes/index.php
+++ Store Clothes/index.php
@@ -4,8 +4,15 @@ require_once 'autoload.php';
 
 if(isset($_GET['controller'])) {
     $nameController = $_GET['controller'].'Controller';
+    $userController = new $nameController();
+
+    if(isset($_GET['action']) && method_exists($userController, $_GET['action'])) {
+        $action = $_GET['action'];
+        $userController->$action();
+    } else {
+        echo "No existe la acción";
+    }
 } else {
     echo 'La pagina no existe';
     exit();
 }
-
