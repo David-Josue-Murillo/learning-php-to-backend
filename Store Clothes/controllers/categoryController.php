a3ab41bcafc269f296f2d0d081b04e488dd56067
David Murillo <david.josue.murillo23@gmail.com>
2024-09-05 23:05:28 -0500

    Estilando tabla de datos

--- Store Clothes/controllers/categoryController.php
+++ Store Clothes/controllers/categoryController.php
@@ -1,7 +1,11 @@
 <?php
+require_once 'model/category.php';
  class CategoryController {
     
     public function index() {
+        $category = new Category();
+        $categories = $category->getCategories();
+
         require_once 'views/category/index.php';
     }
  }
--- Store Clothes/views/category/index.php
+++ Store Clothes/views/category/index.php
@@ -1,2 +1,23 @@
-<h1>Gestionar Categorias</h1>
+<h1 class="title">Gestionar Categorias</h1>
 
+<table class="table">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Acciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($categories as $category) : ?>
+        <tr>
+            <td><?=$category['id']?></td>
+            <td><?=$category['nombre']?></td>
+            <td>
+                <a href="#">Editar</a>
+                <a href="#">Eliminar</a>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
