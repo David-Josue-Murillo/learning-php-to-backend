859d0ca1869df7d948a32b84fa9c41ac34562dbd
David Murillo <david.josue.murillo23@gmail.com>
2024-09-06 20:02:53 -0500

    ADD: Improving visual styles

--- Store Clothes/views/products/featured.php
+++ Store Clothes/views/products/featured.php
@@ -1,27 +1,29 @@
 
-<div id="central">
-    <div class="product">
-        <img src="assets/img/camiseta.png" alt="camiseta">
-        <h2>Camiseta Azul</h2>
-        <p>Precio: 100.00</p>
-        <a class="button" href="#">Comprar</a>
+<div class="container__product">
+    <div id="central">
+        <div class="product">
+            <img src="assets/img/camiseta.png" alt="camiseta">
+            <h2>Camiseta Azul</h2>
+            <p>Precio: 100.00</p>
+            <a class="button" href="#">Comprar</a>
+        </div>
     </div>
-</div>
-
-<div id="central">
-    <div class="product">
-        <img src="assets/img/camiseta.png" alt="camiseta">
-        <h2>Camiseta Azul</h2>
-        <p>Precio: 100.00</p>
-        <a class="button" href="#">Comprar</a>
+    
+    <div id="central">
+        <div class="product">
+            <img src="assets/img/camiseta.png" alt="camiseta">
+            <h2>Camiseta Azul</h2>
+            <p>Precio: 100.00</p>
+            <a class="button" href="#">Comprar</a>
+        </div>
     </div>
-</div>
-
-<div id="central">
-    <div class="product">
-        <img src="assets/img/camiseta.png" alt="camiseta">
-        <h2>Camiseta Azul</h2>
-        <p>Precio: 100.00</p>
-        <a class="button" href="#">Comprar</a>
+    
+    <div id="central">
+        <div class="product">
+            <img src="assets/img/camiseta.png" alt="camiseta">
+            <h2>Camiseta Azul</h2>
+            <p>Precio: 100.00</p>
+            <a class="button" href="#">Comprar</a>
+        </div>
     </div>
 </div>
