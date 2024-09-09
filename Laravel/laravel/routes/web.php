e198cf050e74670fe1a2ea5249fb71922fac2010
David Murillo <david.josue.murillo23@gmail.com>
2024-09-08 23:32:29 -0500

    Parameter handling

--- Laravel/laravel/routes/web.php
+++ Laravel/laravel/routes/web.php
@@ -9,3 +9,7 @@ Route::get('/', function () {
 Route::get('/show-date', function() {
     return view('show-date');
 });
+
+Route::get('/movies/{title?}', function($title = 'No hay una pelicula seleccionada') { 
+    return view('movies', array('title' => $title));
+});
