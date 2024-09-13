15197e7a8e196fc7518f300cc7ac8a0fef60873b
David Murillo <david.josue.murillo23@gmail.com>
2024-09-13 10:15:09 -0500

    Solutions problems the migration

--- Instagram/database/migrations/0001_01_01_000000_create_users_table.php
+++ Instagram/database/migrations/0001_01_01_000000_create_users_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    /*public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
@@ -35,7 +35,7 @@ return new class extends Migration
             $table->longText('payload');
             $table->integer('last_activity')->index();
         });
-    }
+    }*/
 
     /**
      * Reverse the migrations.
--- Instagram/routes/web.php
+++ Instagram/routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Image;
 
 Route::get('/', function () {
+    
+    $images = Image::all();
+    foreach ($images as $image) {
+        # code...
+        echo $image->image_path."<br />";
+        echo $image->description."<br />";
+        echo "<hr />";
+
+    }
+    die();
+    
     return view('welcome');
 });
