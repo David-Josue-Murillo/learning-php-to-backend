261d38b5cc9e341ee5b89a710992816366112aec
David Murillo <david.josue.murillo23@gmail.com>
2024-09-13 09:03:51 -0500

    DB: Create table image

--- Instagram/database.sql
+++ Instagram/database.sql
@@ -15,3 +15,18 @@ CREATE TABLE users(
     remenber_token  varchar(255),
     CONSTRAINT pk_users PRIMARY KEY (id)
 ) ENGINE=InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS image(
+    id              int(255) auto_increment not null,
+    user_id         int(255),
+    image_path      varchar(255),
+    description     text,
+    created_at      datetime, 
+    updated_at      datetime,
+    CONSTRAINT pk_images PRIMARY KEY (id),
+    CONSTRAINT fk_images_users FOREIGN KEY (user_id) REFERENCES users(id)
+) ENGINE=InnoDB;
+
+ 
+ 
