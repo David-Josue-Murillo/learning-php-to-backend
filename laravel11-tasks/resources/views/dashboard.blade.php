7db0b152b4dd49b3d2a2d227121fc4e7fe99edcb
David Murillo <david.josue.murillo23@gmail.com>
2024-09-23 21:03:40 -0500

    Creating a controller to get the data better formatted

--- laravel11-tasks/resources/views/dashboard.blade.php
+++ laravel11-tasks/resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@
                 <div>
                     <h1 class="p-6 text-2xl text-purple-800 font-bold">Bienvenido al gestor de tareas</h1>
                     
-                    @foreach (Auth::user()->tasks as $task)
+                    @foreach ($tasks as $task)
                         <div class="p-6 border-t border-gray-200 border-solid">
                             <p class="pl-6 text-base text-gray-700 font-bold">{{ $task->title }}</p>
                             <p class="pl-6 text-base text-gray-800">{{ $task->description }}</p>
