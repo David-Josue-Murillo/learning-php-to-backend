66b7c4b9adc067203199a2ffb1309ab8cd718b73
David Murillo <david.josue.murillo23@gmail.com>
2024-10-01 22:12:34 -0500

    Learning the methods HTTP

--- api-rest/routes/api.php
+++ api-rest/routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/user', function() {
+    return "Hola mundo";
+});
+
+Route::post('user/{name}', function($name){
+    return "Hola $name, como estas?";
 });
