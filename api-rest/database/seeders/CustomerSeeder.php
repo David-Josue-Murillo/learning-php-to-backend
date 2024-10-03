2f8caaa965e899421c364a101e54ec9f88872654
David Murillo <david.josue.murillo23@gmail.com>
2024-10-02 21:21:43 -0500

    Migrations and seeders execute

--- api-rest/database/seeders/CustomerSeeder.php
+++ api-rest/database/seeders/CustomerSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Customer;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,5 +16,25 @@ class CustomerSeeder extends Seeder
     public function run()
     {
         //
+        Customer::factory()
+            ->count(25)
+            ->hasInvoices(3)
+            ->create();
+
+        Customer::factory()
+            ->count(105)
+            ->hasInvoices(1)
+            ->create();
+
+        Customer::factory()
+            ->count(75)
+            ->hasInvoices(8)
+            ->create();
+
+        Customer::factory()
+            ->count(5)
+            ->hasInvoices(7)
+            ->create();
+                
     }
 }
