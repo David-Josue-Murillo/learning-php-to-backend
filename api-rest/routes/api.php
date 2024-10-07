218c9f011010c473bd901d5311481bdacb713c0c
David Murillo <david.josue.murillo23@gmail.com>
2024-10-06 19:42:32 -0500

    New functionality added for endpoint protection

--- api-rest/routes/api.php
+++ api-rest/routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers'], function() {
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers', 'middleware' => 'auth:sanctum'], function() {
     Route::apiResource('customers', CustomerController::class);
     Route::apiResource('invoices', InvoiceController::class);
     Route::post('invoices/bulk', ['uses' => 'InvoiceController@bulkStore']);
