b1871a1dd3edbe014c678af4cb2ee97720b51523
David Murillo <david.josue.murillo23@gmail.com>
2024-10-06 22:15:29 -0500

    feat: :lipstick: Starter template included
    Styles, effects, designs included

---
+++ e-commerce/app/Http/Controllers/HomeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\View\View;
+
+class HomeController extends Controller
+{
+    public function index(){
+        return view('index');
+    }
+}
