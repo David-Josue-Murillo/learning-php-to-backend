00d4ffb228c72e85bef05c2ac439afcb020d6954
David Murillo <david.josue.murillo23@gmail.com>
2024-10-08 17:53:26 -0500

    feat: :sparkles: Create of all methods of a crud

--- api-students/routes/api.php
+++ api-students/routes/api.php
@@ -3,6 +3,49 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/students', function(){
+    return response()->json([
+        'message' => 'Listado de estudiantes',
+        'status' => 200 
+    ]);
+});
+
+
+Route::get('/student/{id}', function(){
+    return response()->json([
+        'message' => 'Estudiante 1',
+        'status' => 200 
+    ]);
+});
+
+
+Route::post('/student', function(){
+    return response()->json([
+        'message' => 'Creando estudiante',
+        'status' => 200 
+    ]);
+});
+
+
+Route::put('/student/{id}', function(){
+    return response()->json([
+        'message' => 'Modificando todos los datos del estudiante',
+        'status' => 200 
+    ]);
+});
+
+
+Route::patch('/student/{id}', function(){
+    return response()->json([
+        'message' => 'Modificando un dato del estudiante',
+        'status' => 200 
+    ]);
+});
+
+
+Route::delete('/student/{id}', function(){
+    return response()->json([
+        'message' => 'Eliminando estudiante',
+        'status' => 200 
+    ]);
+});
