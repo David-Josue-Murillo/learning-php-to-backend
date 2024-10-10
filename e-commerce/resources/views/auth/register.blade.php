8662d3ff2ea9fb87c662a1d3d131c3914fd766e8
David Murillo <david.josue.murillo23@gmail.com>
2024-10-09 22:49:36 -0500

    style: :lipstick: Adding new fields to the form to create a new user

--- e-commerce/resources/views/auth/register.blade.php
+++ e-commerce/resources/views/auth/register.blade.php
@@ -1,70 +1,94 @@
 @extends('layouts.app')
 @section('content')
 
-<main class="pt-90">
-    <div class="mb-4 pb-4"></div>
-    <section class="login-register container 2xl:w-6/12 xl:w-8/12 lg:w-8/12 md:w-10/12 w-full mx-auto">
-      <ul class="nav nav-tabs mb-5" id="login_register" role="tablist">
-        <li class="nav-item" role="presentation">
-          <a class="nav-link nav-link_underscore active" id="register-tab" data-bs-toggle="tab"
-            href="#tab-item-register" role="tab" aria-controls="tab-item-register" aria-selected="true">Register</a>
-        </li>
-      </ul>
-      <div class="tab-content pt-2" id="login_register_tab_content">
-        <div class="tab-pane fade show active" id="tab-item-register" role="tabpanel" aria-labelledby="register-tab">
-          <div class="register-form">
-            <form method="POST" action="#" name="register-form" class="needs-validation" novalidate="">
-              <div class="form-floating mb-3">
-                <input class="form-control form-control_gray " name="name" value="" required="" autocomplete="name"
-                  autofocus="">
-                <label for="name">Name</label>
-              </div>
-              <div class="pb-3"></div>
-              <div class="form-floating mb-3">
-                <input id="email" type="email" class="form-control form-control_gray " name="email" value="" required=""
-                  autocomplete="email">
-                <label for="email">Email address *</label>
-              </div>
+<main class="pt-90 md:w-1/2 mx-auto">
+    <div class="my-32 md:my-24 2xl:w-8/12 mx-auto flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-white dark:bg-gray-900">
+        <section class="login-register container">
+            <ul class="nav nav-tabs mb-5" id="login_register" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link nav-link_underscore active" id="register-tab" data-bs-toggle="tab"
+                        href="#tab-item-register" role="tab" aria-controls="tab-item-register" aria-selected="true">Register
+                    </a>
+                </li>
+            </ul>
 
-              <div class="pb-3"></div>
+            <div class="tab-content pt-2" id="login_register_tab_content">
+                <div class="tab-pane fade show active" id="tab-item-register" role="tabpanel" aria-labelledby="register-tab">
+                    <div class="register-form">
+                        <form method="POST" action="{{ route('register') }}" name="register-form" class="needs-validation" novalidate="">
+                            @csrf
+                            <div class="form-floating mb-3">
+                                <input class="form-control form-control_gray @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required="" autocomplete="name"
+                                autofocus="">
+                                <label for="name">Name</label>
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
 
-              <div class="form-floating mb-3">
-                <input id="mobile" type="text" class="form-control form-control_gray " name="mobile" value=""
-                  required="" autocomplete="mobile">
-                <label for="mobile">Mobile *</label>
-              </div>
+                            <div class="pb-3"></div>
+                            <div class="form-floating mb-3">
+                                <input id="email" type="email" class="form-control form-control_gray @error('email') is-invalid @enderror" name="email"  value="{{ old('email') }}" required=""
+                                autocomplete="email">
+                                <label for="email">Email address *</label>
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
 
-              <div class="pb-3"></div>
+                            <div class="pb-3"></div>
+                            <div class="form-floating mb-3">
+                                <input id="mobile" type="text" class="form-control form-control_gray @error('email') is-invalid @enderror" name="{{ old('mobile') }}" value=""
+                                  required="" autocomplete="mobile">
+                                <label for="mobile">Mobile *</label>
+                                @error('mobile')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
 
-              <div class="form-floating mb-3">
-                <input id="password" type="password" class="form-control form-control_gray " name="password" required=""
-                  autocomplete="new-password">
-                <label for="password">Password *</label>
-              </div>
+                            <div class="pb-3"></div>
+                            <div class="form-floating mb-3">
+                                <input id="password" type="password" class="form-control form-control_gray @error('password') is-invalid @enderror" name="password" required=""
+                                autocomplete="new-password">
+                                <label for="password">Password *</label>
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            
+                            <div class="pb-3"></div>
+                            <div class="form-floating mb-3">
+                                <input id="password-confirm" type="password" class="form-control form-control_gray"
+                                name="password_confirmation" required="" autocomplete="new-password">
+                                <label for="password">Confirm Password *</label>
+                            </div>
 
-              <div class="form-floating mb-3">
-                <input id="password-confirm" type="password" class="form-control form-control_gray"
-                  name="password_confirmation" required="" autocomplete="new-password">
-                <label for="password">Confirm Password *</label>
-              </div>
+                            <div class="d-flex align-items-center mb-3 pb-2">
+                                <p class="m-0">Your personal data will be used to support your experience throughout this website, to
+                                manage access to your account, and for other purposes described in our privacy policy.</p>
+                            </div>
 
-              <div class="d-flex align-items-center mb-3 pb-2">
-                <p class="m-0">Your personal data will be used to support your experience throughout this website, to
-                  manage access to your account, and for other purposes described in our privacy policy.</p>
-              </div>
+                            <button class="btn btn-primary w-100 text-uppercase" type="submit">Register</button>
 
-              <button class="btn btn-primary w-100 text-uppercase" type="submit">Register</button>
-
-              <div class="customer-option mt-4 text-center">
-                <span class="text-secondary">Have an account?</span>
-                <a href="login.html" class="btn-text js-show-register">Login to your Account</a>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </section>
-  </main>
+                            <div class="customer-option mt-4 text-center">
+                                <span class="text-secondary">Have an account?</span>
+                                <a href="{{ route('login') }}" class="btn-text js-show-register">Login to your Account</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</main>
 
 {{-- <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
