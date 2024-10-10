c7a6fbbe224de4b8dd1121cf2c16bc18ddcbed0b
David Murillo <david.josue.murillo23@gmail.com>
2024-10-09 21:23:43 -0500

    style: :lipstick: Change in some style the login

--- e-commerce/resources/views/layouts/guest.blade.php
+++ e-commerce/resources/views/layouts/guest.blade.php
@@ -15,9 +15,8 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-white dark:bg-gray-900">
-            
-            <div class="bg-gray-900 w-full sm:max-w-md mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-lg">
+        <div class="my-24 flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-white dark:bg-gray-900">
+            <div class="bg-gray-900 w-full sm:max-w-md px-6 py-4 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
