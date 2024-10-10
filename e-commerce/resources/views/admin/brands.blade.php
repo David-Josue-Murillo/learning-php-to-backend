1d84d2ceb89819b13931bc32d531ded8b95da972
David Murillo <david.josue.murillo23@gmail.com>
2024-10-09 23:59:46 -0500

    style: :lipstick: Including logic in the view

--- e-commerce/resources/views/admin/brands.blade.php
+++ e-commerce/resources/views/admin/brands.blade.php
@@ -6,7 +6,7 @@
             <h3>Brands</h3>
             <ul class="breadcrumbs flex items-center flex-wrap justify-start gap10">
                 <li>
-                    <a href="{{  }}">
+                    <a href="{{ route('admin.index') }}">
                         <div class="text-tiny">Dashboard</div>
                     </a>
                 </li>
@@ -48,39 +48,41 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>4</td>
-                                <td class="pname">
-                                    <div class="image">
-                                        <img src="1718066367.html" alt="" class="image">
-                                    </div>
-                                    <div class="name">
-                                        <a href="#" class="body-title-2">Brand4</a>
-                                    </div>
-                                </td>
-                                <td>brand4</td>
-                                <td><a href="#" target="_blank">1</a></td>
-                                <td>
-                                    <div class="list-icon-function">
-                                        <a href="#">
-                                            <div class="item edit">
-                                                <i class="icon-edit-3"></i>
-                                            </div>
-                                        </a>
-                                        <form action="#" method="POST">
-                                            <div class="item text-danger delete">
-                                                <i class="icon-trash-2"></i>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </td>
-                            </tr>
+                            @foreach($brands as $brand)
+                                <tr>
+                                    <td>{{ $brand->id }}</td>
+                                    <td class="pname">
+                                        <div class="image">
+                                            <img src="{{ asset('uploads/brands/'.$brand->image) }}" alt="{{ $brand->name }}" class="image">
+                                        </div>
+                                        <div class="name">
+                                            <a href="#" class="body-title-2">{{ $brand->name }}</a>
+                                        </div>
+                                    </td>
+                                    <td>{{ $brand->slug }}</td>
+                                    <td><a href="#" target="_blank">0</a></td>
+                                    <td>
+                                        <div class="list-icon-function">
+                                            <a href="#">
+                                                <div class="item edit">
+                                                    <i class="icon-edit-3"></i>
+                                                </div>
+                                            </a>
+                                            <form action="#" method="POST">
+                                                <div class="item text-danger delete">
+                                                    <i class="icon-trash-2"></i>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
                 <div class="divider"></div>
                 <div class="flex items-center justify-between flex-wrap gap10 wgp-pagination">
-
+                    {{ $brands->links('pagination::bootstrap-5') }}
                 </div>
             </div>
         </div>
