07411fe3f148df9ff84f8cd55cbc01a5e7096930
David Murillo <david.josue.murillo23@gmail.com>
2024-10-12 18:22:45 -0500

    feat: :sparkles: Get all records from the category table

--- api-rest-php/model/categoria.php
+++ api-rest-php/model/categoria.php
@@ -1,8 +1,14 @@
 <?php
 
-class Categoria{
-    private $id;
-    private $cat_nombre;
-    private $observaciones;
-    private $estado;
+class Categoria extends Database{
+    //Conexion
+    protected $conexion = parent::connect();
+
+    public function getCategorias(){
+        $sql = "SELECT * FROM categoria";
+        $stmt = $this->conexion->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetch();
+        return $result;
+    }
 }
