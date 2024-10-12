0ea862daae6d5a642ef649f7e31361a963533750
David Murillo <david.josue.murillo23@gmail.com>
2024-10-11 23:21:21 -0500

    feat: :sparkles: Method that handles the creation of a new brand added

--- e-commerce/app/Http/Controllers/AdminController.php
+++ e-commerce/app/Http/Controllers/AdminController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Brand;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class AdminController extends Controller
 {
@@ -18,5 +20,25 @@ class AdminController extends Controller
 
     public function addBrand(){
         return view('admin.brand-add');
+    }
+
+    public function brand_store(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required|unique|:brands,slug',
+            'image' => 'mimes:png,jpg,jpeg|max:2048'
+        ]);
+
+        $brand = new Brand();
+        $brand->name = $request->name;
+        $brand->slug = Str::slug($request->slug);
+
+        $image = $request->file('image');
+        $file_extension = $request->file('image')->extension();
+        $file_name = Carbon::now()->timestamp().'.'.$file_extension;
+        
+        $brand->image = $file_name;
+        $brand->save();
+        return redirect()->route('admin.brands')->with('status', 'Brand has been added succefully.');
     }
 }
