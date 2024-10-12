a574b730666861f3535877f6da5657fcfa9367e7
David Murillo <david.josue.murillo23@gmail.com>
2024-10-11 23:05:26 -0500

    feat: :truck: Defining routes to add new brand

--- e-commerce/resources/views/admin/brand-add.blade.php
+++ e-commerce/resources/views/admin/brand-add.blade.php
@@ -6,7 +6,7 @@
             <h3>Brand infomation</h3>
             <ul class="breadcrumbs flex items-center flex-wrap justify-start gap10">
                 <li>
-                    <a href="#">
+                    <a href="{{ route('admin.index') }}">
                         <div class="text-tiny">Dashboard</div>
                     </a>
                 </li>
@@ -14,7 +14,7 @@
                     <i class="icon-chevron-right"></i>
                 </li>
                 <li>
-                    <a href="#">
+                    <a href="{{ route('admin.brands') }}">
                         <div class="text-tiny">Brands</div>
                     </a>
                 </li>
