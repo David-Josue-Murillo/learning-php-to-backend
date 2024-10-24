e3223a734681129c994cb405d2779a2208b22268
David Murillo <david.josue.murillo23@gmail.com>
2024-10-24 18:37:05 -0500

    style: :lipstick: Adding data dynamically to the product view

--- e-commerce/resources/views/admin/products.blade.php
+++ e-commerce/resources/views/admin/products.blade.php
@@ -8,7 +8,7 @@
             <h3 class="text-4xl font-bold ml-14">All Products</h3>
             <ul class="breadcrumbs flex items-center flex-wrap justify-start gap10">
                 <li>
-                    <a href="index.html">
+                    <a href="{{ route('admin.index') }}" class="body-text">
                         <div class="text-tiny">Dashboard</div>
                     </a>
                 </li>
@@ -55,45 +55,49 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>6</td>
-                            <td class="pname">
-                                <div class="image">
-                                    <img src="1718623519.html" alt="" class="image">
-                                </div>
-                                <div class="name">
-                                    <a href="#" class="body-title-2">Product6</a>
-                                    <div class="text-tiny mt-3">product6</div>
-                                </div>
-                            </td>
-                            <td>$128.00</td>
-                            <td>$110.00</td>
-                            <td>SKU7868</td>
-                            <td>Category3</td>
-                            <td>Brand2</td>
-                            <td>Yes</td>
-                            <td>instock</td>
-                            <td>11</td>
-                            <td>
-                                <div class="list-icon-function">
-                                    <a href="#" target="_blank">
-                                        <div class="item eye">
-                                            <i class="icon-eye"></i>
-                                        </div>
-                                    </a>
-                                    <a href="#">
-                                        <div class="item edit">
-                                            <i class="icon-edit-3"></i>
-                                        </div>
-                                    </a>
-                                    <form action="#" method="POST">
-                                        <div class="item text-danger delete">
-                                            <i class="icon-trash-2"></i>
-                                        </div>
-                                    </form>
-                                </div>
-                            </td>
-                        </tr>
+
+                        @foreach ($products as $product)
+                            <tr>
+                                <td>{{  $product->id }}</td>
+                                <td class="pname">
+                                    <div class="image">
+                                        <img src="{{ asset('uploads/products/thumbnails/'.$product->image) }}" alt="{{ $product->name }}" class="image">
+                                    </div>
+                                    <div class="name">
+                                        <a href="#" class="body-title-2">{{ $product->name }}</a>
+                                        <div class="text-tiny mt-3">{{ $product->slug }}</div>
+                                    </div>
+                                </td>
+                                <td>{{ $product->regular_price }}</td>
+                                <td>{{ $product->sale_price }}</td>
+                                <td>{{ $product->SKU }}</td>
+                                <td>{{ $product->category->name }}</td>
+                                <td>{{ $product->brand->name }}</td>
+                                <td>{{ $product->featured ? 'Yes' : 'No' }}</td>
+                                <td>{{ $product->stock_status }}</td>
+                                <td>{{ $product->quantity }}</td>
+                                <td>
+                                    <div class="list-icon-function">
+                                        <a href="#" target="_blank">
+                                            <div class="item eye">
+                                                <i class="icon-eye"></i>
+                                            </div>
+                                        </a>
+                                        <a href="#">
+                                            <div class="item edit">
+                                                <i class="icon-edit-3"></i>
+                                            </div>
+                                        </a>
+                                        <form action="#" method="POST">
+                                            <div class="item text-danger delete">
+                                                <i class="icon-trash-2"></i>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+
                     </tbody>
                 </table>
             </div>
