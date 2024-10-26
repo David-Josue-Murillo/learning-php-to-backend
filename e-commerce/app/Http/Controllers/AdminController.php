aa8691490a6f18ffe3c5eb83f6c1087e14044d1d
David Murillo <david.josue.murillo23@gmail.com>
2024-10-25 22:48:39 -0500

    feat: :sparkles: Method that create new pruducts

--- e-commerce/app/Http/Controllers/AdminController.php
+++ e-commerce/app/Http/Controllers/AdminController.php
@@ -198,4 +198,92 @@ class AdminController extends Controller
 
         return view('admin.product-add', compact('categories', 'brands'));
     }
+
+    public function product_store(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required|unique:products,sluf',
+            'short_description' => 'required',
+            'description' => 'required',
+            'regular_price' => 'required',
+            'sale_price' => 'required',
+            'SKU' => 'required',
+            'stock_status' => 'required',
+            'featured' => 'required',
+            'quantity' => 'required',
+            'image' => 'required|mimes:png,jpg,jpeg|max:2048',
+            'category_id' => 'required',
+            'brand_id' => 'required',
+        ]);
+
+        $product = new Product();
+
+        //Asignación de valores
+        $product->name = $request->name;
+        $product->slug = Str::slug($request->name);
+        $product->short_description = $request->short_description;
+        $product->description = $request->description;
+        $product->regular_price = $request->regular_price;
+        $product->sale_price = $request->sale_price;
+        $product->SKU = $request->SKU;
+        $product->stock_status = $request->stock_status;
+        $product->featured = $request->featured;
+        $product->quantity = $request->quantity;
+        $product->category_id = $request->category_id;
+        $product->brand_id = $request->brand_id;
+
+        $current_timestamp = Carbon::now()->timestamp;
+
+        if($request->hasFile('image')){
+            $image = $request->file('image');
+            $file_extension = $image->extension(); 
+            $image_name = $current_timestamp.'.'.$file_extension;
+
+            $this->GeneratedProductThumbnailImage($image, $image_name);
+            $product->image = $image_name;
+        }
+
+        $gallery_arr = array();
+        $gallery_images = "";
+        $counter = 1;
+
+        if($request->hasFile('images')){
+            $allowedFileExtion = ['jpg', 'png', 'jpeg']; // Extenseions permitidas
+            $files = $request->file('images');
+
+            foreach($files as $file){
+                $getExtension = $file->getClientOriginalExtension();
+                $gcheck = in_array($getExtension, $allowedFileExtion);
+
+                if($gcheck){
+                    $gfileName = $current_timestamp.'-'.$counter.'.'.$getExtension;
+                    $this->GeneratedProductThumbnailImage($file, $gfileName);
+                    array_push($gallery_arr, $gfileName);
+                    $counter++;
+                }
+            }
+
+            $gallery_images = implode(',', $gallery_arr);
+        }
+
+        $product->images = $gallery_images;
+        $product->save();
+
+        return redirect()->route('admin.products')->with('status', 'Product has been created succefully.');
+    }
+
+    public function GeneratedProductThumbnailImage($image, $image_name){
+        $destinationPathThumbnails = public_path('uploads/products/thumbnails');
+        $destinationPath = public_path('uploads/products');
+        $img = Image::read($image->path());
+
+        $img->cover(540,689,"top");
+        $img->resize(540,689,function($constraint){
+            $constraint->aspectRatio();
+        })->save($destinationPath.'/'.$image_name);
+
+        $img->resize(104,104,function($constraint){
+            $constraint->aspectRatio();
+        })->save($destinationPathThumbnails.'/'.$image_name);
+    }
 }
